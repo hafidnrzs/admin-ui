69fd878f79841d1ba520fd32355c8dcb578bcfdd
hafidnrzs <hafidnurazis@gmail.com>
2024-10-28 00:23:12 +0700

    create routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import SignInPage from "./pages/signIn"
 import SignUpPage from "./pages/signUp"
 
 const App = () => {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Halaman Utama</div>,
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+  ])
+
   return (
     <>
-      <SignUpPage />
+      <RouterProvider router={myRouter} />
     </>
   )
 }
