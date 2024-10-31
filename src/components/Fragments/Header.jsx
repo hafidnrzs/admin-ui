c0fa9c36fb0b00043d64758b66207f6d669b3dd7
hafidnrzs <hafidnurazis@gmail.com>
2024-10-31 22:13:54 +0700

    feat: add notification icon

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,4 +1,5 @@
 import Input from "../Elements/LabeledInput/Input";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Header = () => {
   const date = new Date().toDateString().slice(4);
@@ -7,10 +8,12 @@ const Header = () => {
     <header className="border-b-2 p-6 flex items-center justify-between">
       <div className="flex">
         <div className="font-bold text-lg">Username</div>
-        <div className="ms-6 text-gray-03">Oct 17, 2024</div>
+        <div className="ms-6 text-gray-03">{date}</div>
       </div>
       <div className="flex">
-        <div>Icon</div>
+        <div className="self-center">
+          <NotificationsIcon className="text-gray-01 scale-110" />
+        </div>
         <div className="ms-10 hidden sm:block">
           <Input variant="bg-white w-80" />
         </div>
